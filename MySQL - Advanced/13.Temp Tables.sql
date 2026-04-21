79a351bb47baa5dbe68b4ff3ecdb299d45925185
AhmedHaseen <haseen2002hsn@gmail.com>
2026-04-21 21:42:28 +0530

    Completed MySQL temporary tables with practical examples

--- MySQL - Advanced/13.Temp Tables.sql
+++ MySQL - Advanced/13.Temp Tables.sql
@@ -13,3 +13,13 @@ insert into temp_table (first_name , last_name , gender) values
 ("Haseen" , "Ahmed", "Male"),("Afridi",'Khan','Male');
 
 select * from temp_table;
+
+-- Temp table creation mtd 2
+select * from employee_salary;
+
+-- query data that have salary greater thsn or equal to 50k using temp table
+create temporary table temp_salary_50k
+select * from employee_salary
+where salary >= 50000;
+
+select * from temp_salary_50k;
